69726bf3453536bbc4edc1fb186cef4f6ade91ff
Chat Wacharamanotham <chat@ifi.uzh.ch>
2017-12-30 21:07:04 +0100

    Test travis pushing to Chat's github instead

--- _deploy.sh
+++ _deploy.sh
@@ -8,7 +8,7 @@ set -e
 git config --global user.email "robot@transparentstatistics.org"
 git config --global user.name "Robot"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/chatchavan/guidelines.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
